d1432cd9d83321dddb3c393bf356ab34fcd81858
J. DeFrancesco <jdefr89@gmail.com>
2026-08-19 13:52:08 -0400

    Fixed packaging

--- __init__.py
+++ __init__.py
@@ -2,20 +2,22 @@
 
 from __future__ import annotations
 
+from binaryninja import PluginCommand
+
 from .aggressive_dse import run_for_binary, run_for_function
 
+PluginCommand.register_for_function(
+    r"Aggressive DSE\Current Function",
+    "Enable dead-store elimination for variables in the current function",
+    run_for_function,
+)
 
-# PluginCommand.register_for_function(
-#     r"Aggressive DSE\Current Function",
-#     "Enable dead-store elimination for variables in the current function",
-#     run_for_function,
-# )
-#
-# PluginCommand.register(
-#     r"Aggressive DSE\Entire Binary",
-#     "Enable dead-store elimination across the entire binary",
-#     run_for_binary,
-# )
+PluginCommand.register(
+    r"Aggressive DSE\Entire Binary",
+    "Enable dead-store elimination across the entire binary",
+    run_for_binary,
+)
+
 __all__ = (
     "run_for_binary",
     "run_for_function",
