605c1cd512fe6414184b19d7d4446fcfbc243158
J. DeFrancesco <jdefr89@gmail.com>
2026-08-19 13:44:00 -0400

    Fix up registration among other things

--- __init__.py
+++ __init__.py
@@ -1,18 +1,22 @@
 """Binary Ninja plugin entry point."""
 
-from binaryninja import PluginCommand
+from __future__ import annotations
 
 from .aggressive_dse import run_for_binary, run_for_function
 
 
-PluginCommand.register_for_function(
-    r"Aggressive DSE\Current Function",
-    "Enable dead-store elimination for variables in the current function",
-    run_for_function,
-)
-
-PluginCommand.register(
-    r"Aggressive DSE\Entire Binary",
-    "Enable dead-store elimination across the entire binary",
-    run_for_binary,
+# PluginCommand.register_for_function(
+#     r"Aggressive DSE\Current Function",
+#     "Enable dead-store elimination for variables in the current function",
+#     run_for_function,
+# )
+#
+# PluginCommand.register(
+#     r"Aggressive DSE\Entire Binary",
+#     "Enable dead-store elimination across the entire binary",
+#     run_for_binary,
+# )
+__all__ = (
+    "run_for_binary",
+    "run_for_function",
 )
